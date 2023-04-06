1dd5d64f6a449497aab377ec95a09e1bb18ca4b5
shubho-saha <shuvo7242@gmail.com>
2023-04-06 23:30:47 +0600

    recursion function added

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,1 +1,28 @@
 console.log("Recursion");
+
+const person = {
+    name: "mrinmoy",
+    friend: {
+        name: 'sagor',
+        friend: {
+            name: 'saadi'
+        }
+    }
+}
+
+//Recursion Function
+function prinNames(person) {
+    if(person == null) return;
+    console.log(person.name);
+    prinNames(person.friend);
+}
+prinNames(person);
+
+//Another example of Recursion Function
+
+function factorial(number) {
+    if (number <= 1) return 1;
+    return number * factorial(number - 1);
+}
+
+console.log(factorial(3));
