2ebfeba0ddecff6884ef0c74e3a5253b598fc11f
shubho-saha <shuvo7242@gmail.com>
2023-05-04 20:51:58 +0600

    ES6 Modules Practice

---
+++ ES6-modules/js/js2/script.js
@@ -0,0 +1,6 @@
+import print, {sagor, shubho} from "../../user.js";
+
+console.log(shubho)
+console.log(sagor);
+
+print(shubho);
---
+++ ES6-modules/user.js
@@ -0,0 +1,14 @@
+export const sagor = {
+    name: 'Modu Sagor',
+    age: 26
+}
+
+export const shubho = {
+    name: 'Shubho Saha',
+    age: 26
+}
+
+export default function printName(person) {
+    console.log(`Name: ${person.name}\nAge: ${person.age}`);
+}
+
