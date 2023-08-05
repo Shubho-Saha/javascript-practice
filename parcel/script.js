a3109f0ce23a8076dc841c74142a8e091c1346ef
shubho-saha <shuvo7242@gmail.com>
2023-08-05 21:26:55 +0600

    parcel practice session 1 complete

---
+++ parcel/script.js
@@ -0,0 +1,7 @@
+import printUser, {sagor, shubho} from "./user.js";
+import {v4} from "uuid"
+
+printUser(shubho);
+printUser(sagor);
+
+console.log(v4());
---
+++ parcel/user.js
@@ -0,0 +1,11 @@
+export const sagor = {
+    name: 'Modu Sagor'
+}
+
+export const shubho = {
+    name: 'Shubho Saha'
+}
+
+export default function printUser(user) {
+    console.log(user.name);
+}
